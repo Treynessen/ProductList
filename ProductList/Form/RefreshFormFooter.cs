1f5c7df70f700f03b8995d7c4dc20c679fd8008e
tranerius <36367029+tranerius@users.noreply.github.com>
2019-07-02 19:38:48 +0200

    Мелкие правки

--- ProductList/Form/RefreshFormFooter.cs
+++ ProductList/Form/RefreshFormFooter.cs
@@ -10,7 +10,17 @@ public partial class Form1 : Form
             PageInfoLabel.Visible = PreviousPageButton.Visible = NextPageButton.Visible = false;
             MaximumSize = MinimumSize = Size = new Size(816, 634);
         }
-        else if (productsPanelManager.CurrentPageNum != 1)
+        else if (productsPanelManager.CurrentPageNum == 1)
+        {
+            PreviousPageButton.Visible = false;
+            NextPageButton.Location = new Point(11, 588);
+            NextPageButton.Visible = true;
+            MaximumSize = MinimumSize = Size = new Size(816, 670);
+            PageInfoLabel.Visible = true;
+            PageInfoLabel.Width = 276;
+            PageInfoLabel.Text = $"Страница {productsPanelManager.CurrentPageNum} из {productsPanelManager.NumberOfPages}";
+        }
+        else
         {
             PreviousPageButton.Location = new Point(11, 588);
             PreviousPageButton.Visible = true;
@@ -19,16 +29,6 @@ public partial class Form1 : Form
                 NextPageButton.Location = new Point(261, 588);
                 NextPageButton.Visible = true;
             }
-            MaximumSize = MinimumSize = Size = new Size(816, 670);
-            PageInfoLabel.Visible = true;
-            PageInfoLabel.Width = 276;
-            PageInfoLabel.Text = $"Страница {productsPanelManager.CurrentPageNum} из {productsPanelManager.NumberOfPages}";
-        }
-        else if (productsPanelManager.CurrentPageNum == 1)
-        {
-            PreviousPageButton.Visible = false;
-            NextPageButton.Location = new Point(11, 588);
-            NextPageButton.Visible = true;
             MaximumSize = MinimumSize = Size = new Size(816, 670);
             PageInfoLabel.Visible = true;
             PageInfoLabel.Width = 276;
